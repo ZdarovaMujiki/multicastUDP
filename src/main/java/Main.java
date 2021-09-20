04c3e813771ad6262994f75e0c10a81665894e28
ZdarovaMujiki <vvremezovv@mail.ru>
2021-09-20 14:30:43 +0700

    fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,13 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.AbstractMap.SimpleEntry;
-import java.util.concurrent.ConcurrentHashMap;
-
-import static java.lang.Thread.sleep;
+import java.util.HashMap;
 
 public class Main {
-    private static final int SLEEP_DURATION = 1000;
+    private static final int SECONDS_TO_DISCONNECT = 5;
+    private static final int BUFSIZE = 256;
 
-    private static final ConcurrentHashMap<SimpleEntry<InetAddress, String>, Integer> connections = new ConcurrentHashMap<>();
+    private static final HashMap<SimpleEntry<InetAddress, String>, Integer> connections = new HashMap<>();
 
     private static void updateConnections() {
         connections.forEach((key, value) -> connections.put(key, value - 1));
@@ -24,28 +23,52 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println("Multicast group ip and port must be specified");
             return;
         }
-        String groupIP = args[0];
-        int groupPort = Integer.parseInt(args[1]);
-        InetAddress group = InetAddress.getByName(groupIP);
-        MulticastSocket socket = new MulticastSocket(groupPort);
-        socket.joinGroup(group);
+        MulticastSocket socket = null;
+        try {
+            String groupIP = args[0];
+            int groupPort = Integer.parseInt(args[1]);
+            socket = new MulticastSocket(groupPort);
+            InetAddress group = InetAddress.getByName(groupIP);
+            socket.joinGroup(group);
 
-        byte[] message = ManagementFactory.getRuntimeMXBean().getName().getBytes();
-        DatagramPacket packet = new DatagramPacket(message, message.length, group, groupPort);
-        Thread senderThread = new Thread(new PacketSender(socket, packet));
-        senderThread.start();
+            String hostName = ManagementFactory.getRuntimeMXBean().getName();
+            byte[] message = hostName.getBytes();
+            DatagramPacket packet = new DatagramPacket(message, message.length, group, groupPort);
+            Thread senderThread = new Thread(new PacketSender(socket, packet));
+            senderThread.start();
 
-        Thread receiverThread = new Thread(new PacketReceiver(socket, connections));
-        receiverThread.start();
+            byte[] buffer = new byte[BUFSIZE];
+            DatagramPacket receivedPacket = new DatagramPacket(buffer, BUFSIZE);
 
-        while(true) {
-            sleep(SLEEP_DURATION);
-            updateConnections();
+            while(true) {
+                socket.receive(receivedPacket);
+                String senderHostName = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+                SimpleEntry<InetAddress, String> entry = new SimpleEntry<>(receivedPacket.getAddress(), senderHostName);
+                if (!connections.containsKey(entry)) {
+                    connections.put(entry, SECONDS_TO_DISCONNECT);
+                    connections.forEach((pairOfAddressAndHostname, secondsToDisconnect) -> System.out.println(pairOfAddressAndHostname.getValue() + pairOfAddressAndHostname.getKey()));
+                    System.out.println();
+                }
+                else {
+                    connections.put(entry, SECONDS_TO_DISCONNECT);
+                }
+                if (senderHostName.equals(hostName)) {
+                    updateConnections();
+                }
+            }
+        }
+        catch (IllegalArgumentException | IOException e) {
+            System.out.println("bad ip address or port out of range");
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
         }
     }
 }
